dc2c5c8af0ad5de8a17a8c608f894ea3f10195a0
kasidit katcharoen <kasidit.kat@gmail.com>
2025-04-29 18:58:28 +0700

    no message

--- src/app/[locale]/layout.jsx
+++
@@ -1,5 +0,0 @@
-export default async function LocaleLayout({ children, params }) {
-  return (
-    <main id="content">{children}</main>
-  );
-}
--- src/app/[locale]/page.jsx
+++
@@ -1,4 +0,0 @@
-import Home from "@/src/app/[locale]/home/page";
-export default function Page() {
-  return <Home />;
-}
